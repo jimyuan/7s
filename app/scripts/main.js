628ae69ae3afde7e1348c6a110274f0977ed0bbb
Jim Yuan <jim.yuan@wangfan.com>
2014-01-07 18:55:12 +0800

    7s-01:页面框架搭好，event-deep做到一半

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,28 @@
 +function($){ "use strict";
-  /*$(window).on("shake", function(){
-    $("#content").html(s);
-    s++;
-  });*/
-  console.log($.environment().browser)
+  var Seven={
+    createNew:function(){
+      var seven={};
+      seven.transfer=function(){
+        var hash=window.location.hash;
+        if($(hash).length>0){
+          $(".seven-page section").style("z-index", "0");
+          $(hash).style("z-index","10");
+        }
+
+        $(".action-bar>a, #seven-love>a").on("tap", function(e){
+          e.preventDefault();
+          var target=$($(this).attr("href"));
+          $(".seven-page section").style("z-index", "0");
+          target.style("z-index","10");
+        });
+
+      };
+      return seven;
+    }
+  };
+
+  var init=function(){
+    document.body.addEventListener("touchmove", false);
+    Seven.createNew().transfer();
+  }();
 }(window.Quo);

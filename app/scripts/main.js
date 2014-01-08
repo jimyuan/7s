55a1406dabb46c720b8680ecbb911934872bc2aa
Jim Yuan <jim.yuan@wangfan.com>
2014-01-08 19:29:24 +0800

    7s-01: 许愿功能完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,65 @@
 +function($){ "use strict";
-  var Seven={
+  var Common={
     createNew:function(){
-      var seven={};
-      seven.transfer=function(){
+      var common={};
+      common.transfer=function($o){ //单页场景切换
         var hash=window.location.hash;
         if($(hash).length>0){
-          $(".seven-page section").style("z-index", "0");
-          $(hash).style("z-index","10");
+          $("section.slideIn").removeClass("slideIn").addClass("slideOut");
+          $(hash).removeClass("slideOut").addClass("slideIn");
         }
 
-        $(".action-bar>a, #seven-love>a").on("tap", function(e){
-          e.preventDefault();
+        $o.on("tap", function(){
           var target=$($(this).attr("href"));
-          $(".seven-page section").style("z-index", "0");
-          target.style("z-index","10");
+          $("section.slideIn").removeClass("slideIn").addClass("slideOut");
+          target.removeClass("slideOut").addClass("slideIn");
+        }).on("click", function(e){
+          e.preventDefault();
         });
+      }
+      return common;
+    }
+  }
 
-      };
+  var Seven={
+    createNew:function(){
+      var seven={}, c=Common.createNew();
+      seven.init=function(){
+        var $o=$(".seven-page .action-bar>a, #seven-love>a");
+        c.transfer($o);
+      }
       return seven;
     }
   };
 
-  var init=function(){
-    document.body.addEventListener("touchmove", false);
-    Seven.createNew().transfer();
+  var EventDeep={
+    createNew:function(){
+      var eventDeep={}, c=Common.createNew();
+      eventDeep.init=function(){
+        var $o=$("#event-deep-1>.wish-make>a, #event-deep-2>.action-bar>a:first-child");
+        c.transfer($o);
+
+        $(".event-page .wish-count>span").on("tap", function(){
+          $(".wish-content").val("");
+        });
+        $("#event-deep-2>div.action-bar>a:last-child").on("tap", function(){
+          if($(".wish-content").val()==="") {return false;}
+          else{
+            c.transfer($(this));
+            $("#event-deep-1 .wish-text").html($(".wish-content").val().substring(0,30));
+            $("#event-deep-1 .wish-make").style("visibility", "hidden");
+            $("#event-deep-1 .wish-tips").style("visibility", "visible");
+          }
+        }).on("click", function(){return false;});
+
+      };
+      return eventDeep;
+    }
+  };
+
+  var Init=function(){
+    $("section").on("touchmove", function(e){e.preventDefault();});
+    Seven.createNew().init();
+    EventDeep.createNew().init();
   }();
 }(window.Quo);

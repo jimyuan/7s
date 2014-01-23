4a6bed838585d4fb86df064050cc2e821dd76adc
Jim Yuan <jim.yuan@wangfan.com>
2014-01-23 17:01:35 +0800

    7s-10: 完成分享功能和客户修改意见

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,14 @@
   var Common={
     createNew:function(){
       var common={};
+      common.shareText={
+        DEEP: "#7秒爱上绿活泉#想知道我心中爱的愿望吗？开启愿望之门一起来许愿，感受碧欧泉爱的体验。快来和我一起赢取皇牌保湿绿活泉吧！",
+        GLOW: "#7秒爱上绿活泉#爱的滋长需要温暖和阳光，我刚刚参与碧欧泉爱的体验，感受到了暖暖爱意。快来和我一起赢取皇牌保湿绿活泉吧！",
+        BEAUTIFUL: "#7秒爱上绿活泉#大笑是为了证明快乐，苦笑是为了忘却苦涩，而只有微笑是在感受快乐！分享微笑美照，传递浓浓爱意！快来和我一起赢取皇牌保湿绿活泉吧！",
+        TOUCH: "#7秒爱上绿活泉#和TA瞬间的触碰，是否有怦然心动？参与碧欧泉爱的体验，感受那份指尖的悸动！快来和我一起赢取皇牌保湿绿活泉吧！",
+        SCENT: "#7秒爱上绿活泉#在爱的距离中，无论多远，都想向对方传递爱的气息。Kiss手机屏幕，留下爱的印记吧！快来和我一起赢取皇牌保湿绿活泉吧！",
+        REVITALISED: "#7秒爱上绿活泉#打破爱的僵局，有时的确需要勇气，7秒内，成功输入I am fall in love，勇敢说出自己爱的告白吧！快来和我一起赢取皇牌保湿绿活泉吧！"
+      };
       common.transfer=function($o){ //单页场景切换
         var hash=window.location.hash;
         if($(hash).length>0){
@@ -21,8 +29,11 @@
         $o.removeClass("slideOut").addClass("slideIn");
       };
 
-      common.goto=function(){ //event完毕，跳转到分享页面
-        window.setTimeout(function(){window.location="event-over.html";},2000);
+      common.goto=function(EVENT){ //event完毕，跳转到分享页面
+        var wait=arguments[1] || 2000;
+        localStorage.setItem("type",EVENT);
+        localStorage.setItem("text", this.shareText[EVENT]);
+        window.setTimeout(function(){window.location="event-over.html";},wait);
       };
 
       common.passedEvent=function(item, page){
@@ -60,15 +71,64 @@
     createNew:function(){
       var seven={}, c=Common.createNew();
       seven.init=function(){
-        var $o=$(".seven-page .action-bar>a, #seven-love>a");
+        var $o=$(".seven-page .action-bar>a");
         c.transfer($o);
-        $("#seven-love>a").on("click", function(){return false;});
-        this.listHeight();
         this.markEvents();
+        if($("nav.navbar").length>0 && $(".action-bar").length>0){
+          this.listHeight();
+        }
+
+        //分享按钮
+        $(".btn-share").on("click", function(e){
+          e.preventDefault();
+          $.ajax({
+              type: "GET",
+              url: "islogin.ashx",
+              dataType: "json",
+              success: function(a) {
+                if(a.result==="failed"){
+                  window.location = "login.aspx";
+                }
+                else if(a.result==="success"){
+                  window.location = "share.html";
+                } 
+              },
+              error: function() {
+                console.log("failed");
+              }
+          });
+        });
+
+        //提交申领
+        $(".apply-submit").on("click", function(e){
+          e.preventDefault();
+          var f=$(this).closest("form");
+          var fullname=$("#fullname").val(), mobile=$("#mobile").val(), email=$("#email").val();
+          var pe=/^\w(\w*\.*-*)*@([\w-]+\.)+\w{2,4}$/;
+          var pm=/^\d{11}$/;
+
+          if(fullname==="" || mobile==="" || email===""){
+            alert("请将表单填写完整");
+            return false;
+          }
+          if(!pe.test(email) || !pm.test(mobile)){
+            alert("请填写正确的格式");
+            return false;
+          }
+          $.post(
+            "apply.ashx",
+            {"name":fullname, "mobile":mobile, "email":email}, 
+            function(e){
+              if(e.result==="success"){
+                alert("提交成功！");
+                window.location="event-list.html";                
+              }
+            }, "json");
+        });
       };
       seven.listHeight=function(){
         var h=window.innerHeight-$("nav.navbar").height()-$(".action-bar").height();
-        $(".seven-page .event-link ul").style("height",h+"px").children().style("height",h/3+"px");
+        $("#event-list .event-link ul").style("height",h+"px").children().style("height",h/3+"px");
       };
       seven.markEvents=function(){
         for(var i=1; i<=6; i++){
@@ -86,20 +146,21 @@
       var eventDeep={}, c=Common.createNew();
       eventDeep.init=function(){
         if($("#event-deep-1").length>0){
-          var $o=$("#event-deep-1>.wish-make>a, #event-deep-2>.action-bar>a:first-child");
+          var $o=$("#event-deep-1>.wish-make>a");
           c.transfer($o);
 
           $(".event-page .wish-count>span").on("tap", function(){
             $(".wish-content").val("");
           });
           //确定按钮
-          $("#event-deep-2>div.action-bar>a:last-child").on("tap", function(){
+          $(".submit-wish").on("click", function(e){
+            e.preventDefault();
             if($(".wish-content").val()==="") {return false;}
             else{
               localStorage.setItem("wish", $(".wish-content").val());
               window.location="event-deep2.html";
             }
-          }).on("click", function(){return false;});
+          });
         }
 
         if($("#event-deep-3").length>0){
@@ -115,20 +176,17 @@
       };
 
       var shake=function(){
-        var c = 0;
+        var s = 0;
         var h = $(".wish-full").height();
         $(window).on("shake", function(e){
           e.preventDefault();
-          // if($("#event-deep-2")) {
-            $("#event-deep-2").remove();
-          // }
-          if(c<7){
-            $(".wish-full").style("background-position", "0 -"+c*h+"px");
-            c++;
+          if(s<7){
+            $(".wish-full").style("background-position", "0 -"+s*h+"px");
+            s++;
           }
-          if(c===7){
-            Common.createNew().passedEvent("event01","event-deep.html");
-            Common.createNew().goto();
+          if(s===7){
+            c.passedEvent("event01","event-deep.html");
+            c.goto("DEEP");
           }
         });
       };
@@ -149,7 +207,7 @@
           if(tapCount===7){
             $(".radiance").style("visibility", "visible");
             Common.createNew().passedEvent("event02", "event-glow.html");
-            Common.createNew().goto();
+            Common.createNew().goto("GLOW");
           }
         });
       };
@@ -161,6 +219,7 @@
     createNew:function(){
       var eventBeauty={};
       eventBeauty.init=function(){
+        var r=0;
         $("#file").on("change", function(e){
           $(".progress").style("display", "block");//.children().style("width", "20%");
           if (window.File && window.FileReader) {
@@ -172,22 +231,40 @@
               reader.onprogress =function(e){
                 if (e.lengthComputable) {
                   var percentLoaded = Math.round((e.loaded / e.total) * 100);
-                  // Increase the progress bar length.
-                  // console.log(percentLoaded);
                   $(".progress-bar").style("width", percentLoaded+"%");
                 }
               };
               reader.onloadstart = function(e) {
-                //初始化进度条
-                // $(".progress").style("display", "block").children().style("width", "0%");
               };
               reader.onload = function(e){
-                // document.getElementById("text").style.backgroundImage="url('"+e.target.result+"')";
-                $("#text").html("").style("backgroundImage", "url('"+e.target.result+"')");
+                var img=e.target.result;
+                $(".rotate-pic").style("display", "block");
+                $("#text").html("").style("backgroundImage", "url('"+img+"')");
                 $(".progress").style("display", "none");
+                $(".picUpload").on("click", function(){
+                  $(".progress").style("display", "block");
+                  $.post("upload_stream.ashx",{image:img}, function(r){
+                    $(".progress").style("display", "none");
+                    if(r.result==="success"){
+                      alert("上传成功");
+                      Common.createNew().passedEvent("event03", "event-beautiful.html");
+                      localStorage.setItem("pic", r.jsonResponse);
+                      Common.createNew().goto("BEAUTIFUL",20);
+                    }
+                  }, "json");
+                });
               };
             }
           }
+        });
+
+        $(".rotate-pic>img:first-child").on("tap", function(){
+          r-=90;
+          $("#text").style("-webkit-transform", "rotate("+r+"deg)");
+        });
+        $(".rotate-pic>img:last-child").on("tap", function(){
+          r+=90;
+          $("#text").style("-webkit-transform", "rotate("+r+"deg)");
         });
       };
       return eventBeauty;
@@ -198,11 +275,11 @@
     createNew:function(){
       var eventTouch={};
       eventTouch.init=function(){
-        $(".pinch").on("pinchIn", function(){
+        $(".pinch-zone").on("pinchIn", function(){
           $(".big-glow").style("visibility", "visible");
           $(".pinch").style("visibility", "hidden");
           Common.createNew().passedEvent("event04","event-touch.html");
-          Common.createNew().goto();
+          Common.createNew().goto("TOUCH");
         });
       };
       return eventTouch;
@@ -217,7 +294,7 @@
           if(e.touches.length===2){
             $(this).addClass("kissed");
             Common.createNew().passedEvent("event05","event-scent.html");
-            Common.createNew().goto();
+            Common.createNew().goto("SCENT");
           }
         });
       };
@@ -228,6 +305,7 @@
   var EventRev={
     createNew:function(){
       var eventRev={}, str="I am fall in love", s=0, t;
+      var c=Common.createNew();
       eventRev.init=function(){
         $(".text-input").on("keydown",function(){
           if(s===0){
@@ -244,8 +322,8 @@
         });
 
         $("#event-revitalised-1>img").on("tap", function(){
-          Common.createNew().passedEvent("event06","event-revitalised.html");
-          window.location="event-over.html";
+          c.passedEvent("event06","event-revitalised.html");
+          c.goto("REVITALISED", 10);
         });
       };
       return eventRev;
@@ -284,9 +362,6 @@
         $(".video-area").on("tap", function(e){
           e.preventDefault();
           c.GAEvent("click", "7秒初爱视频播放");
-        });
-        $("#seven-love>a").on("tap", function(){
-          c.GAEvent("tap", "进入体验");
         });
         $("#seven-love>action-bar>a").on("tap", function(){
           c.GAEvent("tap","活动说明");
